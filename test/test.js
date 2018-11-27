101c1de99344278dd7b5ea8d335df4dc3075da20
Sam Jordan <sam.jordan@shopify.com>
2018-11-26 21:31:13 -0500

    add revinfo tests using tsv data

--- test/test.js
+++ test/test.js
@@ -1,15 +1,23 @@
+const fs = require('fs');
 const path = require('path');
 const { expect } = require('chai');
+const csv = require('neat-csv');
 const pirev = require('..');
 
 function testDataPath(filename) {
   return path.join(__dirname, 'data', filename);
 }
 
+function readTsvData(filename) {
+  return csv(fs.createReadStream(filename), { separator: '\t' });
+}
+
 const MISSING_FILE = testDataPath('missing');
 const CPUINFO_PI1 = testDataPath('cpuinfo_pi1');
 const CPUINFO_PI3 = testDataPath('cpuinfo_pi3');
 const CPUINFO_MBP = testDataPath('cpuinfo_mbp');
+const OLD_CODES = testDataPath('codes_old.tsv');
+const NEW_CODES = testDataPath('codes_new.tsv');
 
 describe('getInfo', () => {
   describe('missing file', () => {
@@ -113,12 +121,32 @@ describe('getInfoSync', () => {
       }).to.throw(ReferenceError, 'No revision code found');
     });
   });
+});
 
-  describe('missing file', () => {
-    it('should fail to find the file', () => {
-      expect(() => {
-        pirev.getInfoSync(MISSING_FILE);
-      }).to.throw(Error);
+describe('revinfo', () => {
+  it('correctly parses old codes', async () => {
+    const data = await readTsvData(OLD_CODES);
+
+    data.forEach(({code, model, revision, ram, manufacturer}) => {
+      const info = pirev.getRevInfo(code);
+
+      expect(info).to.have.property('type', model);
+      expect(info).to.have.property('memory', ram);
+      expect(info).to.have.property('revision', parseFloat(revision));
+      expect(info).to.have.property('manufacturer', manufacturer);
+    });
+  });
+
+  it('correctly parse new codes', async () => {
+    const data = await readTsvData(NEW_CODES);
+
+    data.forEach(({code, model, revision, ram, manufacturer}) => {
+      const info = pirev.getRevInfo(code);
+
+      expect(info).to.have.property('type', model);
+      expect(info).to.have.property('memory', ram);
+      expect(info).to.have.property('revision', parseFloat(revision));
+      expect(info).to.have.property('manufacturer', manufacturer);
     });
   });
 });
